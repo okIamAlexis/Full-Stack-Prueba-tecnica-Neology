e62870b427c6abd8a86b20b8e08cf0ae9fe4b4f4
Alexis Molinero <molineroc@globalhitss.com>
2026-08-14 15:10:35 -0600

    feat: estructura inicial backend y frontend

---
+++ parking-backend/src/main/java/com/neology/parking_backend/ParkingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.neology.parking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingBackendApplication.class, args);
+	}
+
+}
---
+++ parking-backend/src/test/java/com/neology/parking_backend/ParkingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neology.parking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
